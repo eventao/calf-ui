9ce2a9b78de09777c0862589984ac30bc3cc96ce
evenyuan <330293577@qq.com>
2018-03-11 19:02:36 +0800

    添加组件

--- src/pages/list-test/list-test.ts
+++ src/pages/list-test/list-test.ts
@@ -30,14 +30,21 @@ export class ListTestPage {
     this.indexes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ#'.split('');
     const b = Math.ceil(1000 / 27);
     for (let i = 0; i < 1000; i++) {
-      let item = {};
+      let item = {
+        groupName:"",
+        name:""
+      };
       if (i % b === 0) {
         takeIndex++;
-        item['groupName'] = this.indexes[takeIndex];
+        item.groupName = this.indexes[takeIndex];
       } else {
-        item['name'] = `${this.indexes[takeIndex]}--${i}`;
+        if (i % 12 === 0){
+          item.name = `${this.indexes[takeIndex]}--${i}`;
+        }
       }
-      this.clientItemList.push(item);
+      if(item.groupName || item.name){
+        this.clientItemList.push(item);
+      }
     }
   }
 
@@ -71,38 +78,51 @@ export class ListTestPage {
   drawContact(zr) {
     let maxX = this.vw(95);
     let y = this.vw(15);
+    let yGroup = this.vw(13);
     this.clientItemsGroup = new zrender.Group();
     this.clientItemsGroup.position[0] = 0;
     this.clientItemsGroup.position[1] = 0;
-    for(let i = 0; i < 88; i++){
+    this.clientItemList.forEach((item,i) => {
       let renderG = new zrender.Group();
       renderG.position[0] = 0;
       renderG.position[1] = y * i;
+      let drawY,graphText;
+      if(item.groupName){
+        drawY = yGroup;
+        graphText = item.groupName;
+      }else{
+        drawY = y;
+        graphText = item.name;
+      }
       let rect = new zrender.Rect({
         shape: {
           x: this.vw(0),
           y: this.vw(0),
           width:maxX,
-          height:y
+          height:drawY
         },
         style:{
           fill:"#fff",
-          text:"龙须然",
+          text:graphText,
           textFill:"#333"
         }
       });
       let bottomBorder = new zrender.Line({
         shape:{
           x1:this.vw(0),
-          y1:this.vw(15),
+          y1:this.vw(14),
           x2:maxX,
-          y2:this.vw(15)
+          y2:this.vw(14)
+        },
+        style:{
+          lineWidth:0.2
         }
       });
       renderG.add(rect);
       renderG.add(bottomBorder);
       this.clientItemsGroup.add(renderG);
-    }
+
+    });
     zr.add(this.clientItemsGroup);
   }
 
@@ -155,6 +175,14 @@ export class ListTestPage {
       startData.isIndexDown = false;
       this.draggingContentData.isDragging = false;
     });
+
+    setTimeout(() => {
+      const canvas = document.querySelector('.render-wrapper canvas');
+      canvas.addEventListener('swipe', e => {
+        console.log('swipe');
+      });
+    },300);
+
     this.drawIndex(zr);
     this.drawContact(zr);
   }
@@ -162,10 +190,14 @@ export class ListTestPage {
   draggingContent(e){
     if(this.draggingContentData.isDragging){
       const subx = e.offsetY - this.draggingContentData.point.y;
-
       let sourceY = this.clientItemsGroup.position[1];
-      this.clientItemsGroup.attr('position',[0,sourceY + subx]);
-
+      let targetY = sourceY + subx;
+      if(subx > 0 && targetY < 0){
+        this.clientItemsGroup.attr('position',[0,sourceY + subx]);
+      }
+      if(subx < 0){
+        this.clientItemsGroup.attr('position',[0,sourceY + subx]);
+      }
       this.draggingContentData.point.x = e.offsetX;
       this.draggingContentData.point.y = e.offsetY;
     }
